eac07c68d03eca7079585c02e8bc9150c63af1b6
CherryBurst103 <145829403+CherryBurst103@users.noreply.github.com>
2023-09-23 19:36:42 -0700

    Update Main.php

--- src/Terpz710/SoupPvP/Main.php
+++ src/Terpz710/SoupPvP/Main.php
@@ -3,7 +3,8 @@
 namespace Terpz710\SoupPvP;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener {
@@ -12,13 +13,17 @@ class Main extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function onPlayerItemConsume(PlayerItemConsumeEvent $event) {
+    public function onPlayerInteract(PlayerInteractEvent $event) {
         $player = $event->getPlayer();
+        $item = $event->getItem();
 
-        if ($event->getItem() instanceof \pocketmine\item\MushroomStew) {
-            $event->cancel();
-            $player->sendMessage("Healed §b4§r total hearts!");
-            $player->setHealth($player->getHealth() + 4);
+        if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_AIR || $event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
+            if ($event->getItem() instanceof \pocketmine\item\MushroomStew) {
+                $event->cancel();
+                $player->setHealth($player->getHealth() + 4);
+                $player->sendMessage("Healed §b4§r total hearts!");
+                $player->getInventory()->removeItem($item);
+            }
         }
     }
 }
